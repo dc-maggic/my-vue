1729855eed413ab64a52487b611f1187d3c28068
dc-maggic <dc-maggic@outlook.com>
2023-03-16 01:16:34 +0800

    新加鼠标移动点光源

--- readme.md
+++ readme.md
@@ -32,6 +32,25 @@
   * three3 学习加载模型
     * 加载器 **GLTFLoader** 等
         > 由于不知道加载模型加载成功没有，就去看了源码。发现GLTFLoader 是运用了 FileLoader 函数，其实就是运用了fetch请求方法。对比了three以前的版本，他们的区别是旧版使用的是XMLHttpRequest，而新版运用了 fetch 方法。fetch 没有 progress 的方法与，就利用 ProgressEvent 方法去测量请求过程的速度等，而 XMLHttpRequest 是本身自带 progress 方法的。
+
         > 检查加载成功后，还是找不到模型。就调试了下相机、灯光的位置才发现了。
+
+        > 不知道为什么我导入的模型，只写一行不行，要加个动画一直渲染才行。因为我看很多博主写的都一行也行，不知道是不是模型的问题。并且，我用几何体时也可以。
+
+          <!-- 只有这句 -->
+          renderer.render(scene, camera);
+          <!-- 加了动画 -->
+          const animate = () => {
+            requestAnimationFrame(tick);
+            renderer.render(scene, camera);
+            controls.update();
+          }
+          animate();
+        
+        (```)
+
+        > 加
     * 控制器 **OrbitControls** 等
       > three 在用多个 scene 情况下，我用 OrbitControls 也没办法生效。发现只用一个 scene 时就生效了。有待考证如果在多个 scene 情况下该怎么转动！
+      > 加了控制器以后模型的位置发生变化，我看相机的 position 没变。
+    * 灯光 **DirectionalLight**、**PointLight** 等
--- src/pages/three3/app.vue
+++ src/pages/three3/app.vue
@@ -16,9 +16,6 @@
 </template>
 <script>
 import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import { Clock, Scene, LoadingManager, WebGLRenderer, sRGBEncoding, Group, PerspectiveCamera, DirectionalLight, PointLight, MeshPhongMaterial, AmbientLight } from 'three';
-import { TWEEN } from 'three/examples/jsm/libs/tween.module.min.js';
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 export default{
@@ -42,36 +39,38 @@ export default{
 		renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 		// renderer.autoClear = true;
 		// outputEncoding 属性控制输出渲染编码。
-		renderer.outputEncoding = sRGBEncoding;
+		renderer.outputEncoding = THREE.sRGBEncoding;
 
+		// 做2个渲染器就不给旋转了！！
 		// 初始化渲染器2
-		const canvas2 = this.$refs.detail
-		const renderer2 = new THREE.WebGLRenderer({ 
-			canvas: canvas2,
-			antialias: false
-		});
-		renderer2.setSize(sizes.width, sizes.height);
-		renderer2.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-		renderer2.autoClear = true;
+		// const canvas2 = this.$refs.detail
+		// const renderer2 = new THREE.WebGLRenderer({ 
+		// 	canvas: canvas2,
+		// 	antialias: false
+		// });
+		// renderer2.setSize(sizes.width, sizes.height);
+		// renderer2.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+		// renderer2.autoClear = true;
 
 		// 初始化场景
 		const scene = new THREE.Scene();
 
 		// 初始化相机
-		const cameraGroup = new Group();
+		const cameraGroup = new THREE.Group();
 		scene.add(cameraGroup);
-		const camera = new PerspectiveCamera(35, sizes.width / sizes.height, 1, 100)
-		camera.position.set(0 ,2, 10);
+		const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height, 1, 100)
+		camera.position.set(0 ,2, 15);
 		cameraGroup.add(camera);
 
-
-
 		// 直射光
-		const directionLight = new DirectionalLight(0xffffff, .8);
-		directionLight.position.set(5, 5, 7.5);
+		const directionLight = new THREE.DirectionalLight(0xffffff, .8);
+		directionLight.position.set(-50, 19, 20);
 		scene.add(directionLight);
-
-
+		// 添加灯光
+		const light = new THREE.PointLight( 'orange', 2.7, 4, 3 );
+		light.position.set( 2, 0, 2);
+		scene.add(light);
+		
 		const dracoLoader = new DRACOLoader();
 		dracoLoader.setDecoderPath( '/draco/' );
 		dracoLoader.setDecoderConfig({ type: 'js' });
@@ -80,12 +79,10 @@ export default{
 		// 模型加载
 		let oldMaterial;
 		loader.load('/model/statue.glb', function (gltf) {
-
-				console.log(gltf)
 			gltf.scene.traverse((obj) => {
 				if (obj.isMesh) {
 					oldMaterial = obj.material;
-					obj.material = new MeshPhongMaterial({
+					obj.material = new THREE.MeshPhongMaterial({
 						shininess: 100
 					})
 				}
@@ -94,12 +91,35 @@ export default{
 			oldMaterial.dispose();
 			renderer.renderLists.dispose();
 		});
-		const tick = () => {
+		// 鼠标事件 灯光移动
+		this.$cursor= {x:0, y:0}
+		document.addEventListener('mousemove', this.mouseEvent, false);
+
+		// 动画
+		const clock = new THREE.Clock()
+		let previousTime = 0;
+		const animate = () => {
+			const elapsedTime = clock.getElapsedTime(),
+				deltaTime = elapsedTime - previousTime;
+			previousTime = elapsedTime;
+			const { y: parallaxY, x: parallaxX } = this.$cursor;
+			light.position.y -= (parallaxY * 9 + light.position.y - 2) * deltaTime;
+			light.position.x += (parallaxX * 8 - light.position.x) * 2 * deltaTime;
+			requestAnimationFrame(animate);
 			renderer.render(scene, camera);
-			requestAnimationFrame(tick);
 		}
-		tick();
-    }
+		animate();
+    },
+	methods: {
+		mouseEvent(e) {
+			e.preventDefault();
+			this.$cursor.x = e.clientX / window.innerWidth - .5;
+			this.$cursor.y = e.clientY / window.innerHeight - .5;
+		}
+	},
+	beforeUnmount() {
+		document.removeEventListener('mousemove', this.mouseEvent)
+	}
 }
 
 </script>
